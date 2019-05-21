0e6c56cac8f05bbf714a4ee123bfc2fa4e999502
saishibu38380 <saishibu38380@gmail.com>
2019-05-21 13:55:49 +0530

    Updated user input

--- pythonUserInput.py
+++ pythonUserInput.py
@@ -1,1 +1,6 @@
-##
+#!/usr/bin/python
+#(c)Sai Shibu
+#input function takes input from user and stores it to a variable
+name = input("Enter your name : ") 
+#Print function is used to print the value stored in the variable
+print(name)
